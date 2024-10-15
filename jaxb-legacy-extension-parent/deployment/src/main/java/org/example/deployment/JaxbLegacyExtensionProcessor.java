691b3ce0b4b2ef256f4e74307ddc04e6cb5ae7f9
damien <damien.clementdhuart@gmail.com>
2024-10-16 00:06:22 +0200

    register for native reflection javax XmlRootElement ... maybe I should do the same for Jakarta one ? not in this sample but I should do it :)

--- jaxb-legacy-extension-parent/deployment/src/main/java/org/example/deployment/JaxbLegacyExtensionProcessor.java
+++ jaxb-legacy-extension-parent/deployment/src/main/java/org/example/deployment/JaxbLegacyExtensionProcessor.java
@@ -5,7 +5,9 @@ import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.BytecodeTransformerBuildItem;
 import io.quarkus.deployment.builditem.CombinedIndexBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
 import org.jboss.jandex.IndexView;
 
 public class JaxbLegacyExtensionProcessor {
@@ -18,14 +20,35 @@ public class JaxbLegacyExtensionProcessor {
     }
 
     @BuildStep
-    void transformClasses(final CombinedIndexBuildItem combinedIndexBuildItem,
-                          final BuildProducer<BytecodeTransformerBuildItem> transformers) {
+    void transformAllJavaxToJakartaClasses(final CombinedIndexBuildItem combinedIndexBuildItem,
+                                           final BuildProducer<BytecodeTransformerBuildItem> transformers) {
         final IndexView index = combinedIndexBuildItem.getIndex();
-
         for (final ClassInfo classInfo : index.getKnownClasses()) {
             final String className = classInfo.name().toString();
 
             transformers.produce(new BytecodeTransformerBuildItem(className, (className2, classVisitor) -> new ImportTransformer(classVisitor)));
         }
+    }
+
+    @BuildStep
+    void registerNativeXmlRootElementClasses(final CombinedIndexBuildItem combinedIndexBuildItem,
+                                             final BuildProducer<ReflectiveClassBuildItem> reflectiveClassBuildItemBuildProducer) {
+        final IndexView index = combinedIndexBuildItem.getIndex();
+
+        final String[] classesHavingXmlRootElement = index.getKnownClasses()
+                .stream()
+                .filter(classInfo -> classInfo.hasAnnotation(DotName.createSimple("javax.xml.bind.annotation.XmlRootElement")))
+                .map(ClassInfo::name)
+                .map(DotName::toString)
+                .toArray(String[]::new);
+        reflectiveClassBuildItemBuildProducer.produce(
+                ReflectiveClassBuildItem.builder(classesHavingXmlRootElement)
+                        .constructors(true)
+                        .methods(true)
+                        .fields(true)
+                        .weak(true)
+                        .serialization(true)
+                        .build()
+        );
     }
 }
--- jaxb-legacy-extension-parent/integration-tests/src/main/java/org/example/NativeFix.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection(targets = {Person.class})
-public class NativeFix {
-}
